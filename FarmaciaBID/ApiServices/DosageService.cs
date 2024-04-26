b441ad164e2e7083793af6322fc251fb92d79140
Joshua Campos Gutiérrez <gutierrez25g@gmail.com>
2024-04-25 20:34:44 -0600

    create dosages ready

--- FarmaciaBID/ApiServices/DosageService.cs
+++ FarmaciaBID/ApiServices/DosageService.cs
@@ -6,7 +6,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net;
 
 namespace FarmaciaBID.ApiServices
 {
@@ -40,6 +40,41 @@ namespace FarmaciaBID.ApiServices
                 }
 
                 
+            }
+        }
+
+
+        public async Task CreateDosage(Dosage newDosage)
+        {
+            using (var client = new HttpClient())
+            {
+                System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                client.BaseAddress = new Uri(apiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                string json = JsonConvert.SerializeObject(newDosage);
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                // Realizar la solicitud POST
+                HttpResponseMessage response = await client.PostAsync("/api/Dosificaciones", content);
+
+                // Verificar si la respuesta indica éxito basado en el código de estado
+                if (response.IsSuccessStatusCode)
+                {
+                    // Respuesta exitosa
+                    return;
+                }
+                else if (response.StatusCode == HttpStatusCode.Conflict)
+                {
+                    // Manejar el caso de duplicado
+                    throw new Exception("Error al crear el donante. Ya existe un registro con los mismos datos.");
+                }
+                else
+                {
+                    // Manejar otros casos de error
+                    throw new Exception($"Error al crear el donante. Código de estado: {response.StatusCode}");
+                }
             }
         }
     }
--- FarmaciaBID/Controllers/DosagesController.cs
+++ FarmaciaBID/Controllers/DosagesController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Threading.Tasks;
-
+using FarmaciaBID.Models;
 
 namespace FarmaciaBID.Controllers
 {
@@ -19,5 +19,40 @@ namespace FarmaciaBID.Controllers
 
             return View(employe);
         }
+
+
+        public async Task<ActionResult> CreateDosages()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateDosages(Dosage NewDosage)
+        {
+            try
+            {
+                // Verificar si el modelo es válido antes de intentar guardarlo en la base de datos
+                if (ModelState.IsValid)
+                {
+                    // Lógica para guardar la nueva dosificación en la base de datos utilizando el servicio
+                    await dosageService.CreateDosage(NewDosage);
+
+                    // Redireccionar a una vista de éxito o a la lista de dosificaciones
+                    return RedirectToAction("ViewDosage");
+                }
+                return View(NewDosage);
+            }
+            catch (Exception ex)
+            {
+                // Manejar cualquier excepción que pueda ocurrir al llamar al servicio
+                ModelState.AddModelError("", "Error al intentar guardar la dosificación. Por favor, inténtelo de nuevo más tarde.");
+                return RedirectToAction("Error");
+            }
+        }
+
+
+
     }
 }
