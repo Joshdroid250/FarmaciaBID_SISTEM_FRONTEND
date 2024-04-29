b5c821c353f18fc8adefc63043bd56ad616eb9c0
Joshua Campos <jcampos@AD.PRODEL.ORG.NI>
2024-04-29 09:31:07 -0600

    create patients working, datepicker with problemns

--- FarmaciaBID/ApiServices/PatientsService.cs
+++ FarmaciaBID/ApiServices/PatientsService.cs
@@ -42,5 +42,39 @@ namespace FarmaciaBID.ApiServices
 
             }
         }
+
+        public async Task CreatePacientes(Patients newPatient)
+        {
+            using (var client = new HttpClient())
+            {
+                System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                client.BaseAddress = new Uri(apiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                string json = JsonConvert.SerializeObject(newPatient);
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                // Realizar la solicitud POST
+                HttpResponseMessage response = await client.PostAsync("/api/Pacientes", content);
+
+                // Verificar si la respuesta indica éxito basado en el código de estado
+                if (response.IsSuccessStatusCode)
+                {
+                    // Respuesta exitosa
+                    return;
+                }
+                else if (response.StatusCode == HttpStatusCode.Conflict)
+                {
+                    // Manejar el caso de duplicado
+                    throw new Exception("Error al crear el donante. Ya existe un registro con los mismos datos.");
+                }
+                else
+                {
+                    // Manejar otros casos de error
+                    throw new Exception($"Error al crear el donante. Código de estado: {response.StatusCode}");
+                }
+            }
+        }
     }
 }
--- FarmaciaBID/Controllers/PatientsController.cs
+++ FarmaciaBID/Controllers/PatientsController.cs
@@ -6,6 +6,7 @@ using FarmaciaBID.Models;
 using FarmaciaBID.ApiServices.ApiConfig;
 using System.Net.Http;
 using FarmaciaBID.ApiServices;
+using System;
 
 namespace FarmaciaBID.Controllers
 {
@@ -18,6 +19,38 @@ namespace FarmaciaBID.Controllers
             var employe = await patientsService.GetAllPacientes();
 
             return View(employe);
+        }
+
+
+        public async Task<ActionResult> CreatePatients()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreatePatients(Patients newPatient)
+        {
+            try
+            {
+                // Verificar si el modelo es válido antes de intentar guardarlo en la base de datos
+                if (ModelState.IsValid)
+                {
+                    // Lógica para guardar la nueva dosificación en la base de datos utilizando el servicio
+                    await patientsService.CreatePacientes(newPatient);
+
+                    // Redireccionar a una vista de éxito o a la lista de dosificaciones
+                    return RedirectToAction("ViewPatients");
+                }
+                return View(newPatient);
+            }
+            catch (Exception ex)
+            {
+                // Manejar cualquier excepción que pueda ocurrir al llamar al servicio
+                ModelState.AddModelError("", "Error al intentar guardar la dosificación. Por favor, inténtelo de nuevo más tarde.");
+                return RedirectToAction("Error");
+            }
         }
     }
 }
